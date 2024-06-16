01f588a756f04dc2a32e093f7cb4a14ac63af5b1
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-06-16 20:01:36 +0530

    Update Analysing.py

--- Analysing.py
+++ Analysing.py
@@ -3,5 +3,8 @@ import pandas as pd #importing pandas
 #creating a variable that contains scores 
 scores=pd.read_csv('Aim Traning.csv')
 
+#printing data at 22nd may 2024
+print (scores[scores["Date"]=="22nd May 2024"]
+
 #printing the scores
 print(scores)
