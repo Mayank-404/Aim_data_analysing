80006fd266f1f5fd2cd120a950bf8e616876d599
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-06-17 16:26:06 +0530

    Update Analysing.py

--- Analysing.py
+++ Analysing.py
@@ -3,8 +3,13 @@ import pandas as pd #importing pandas
 #creating a variable that contains scores 
 scores=pd.read_csv('Aim Traning.csv')
 
+#calculating mean of 2nd coloum 
+mean=scores['Smooth Strafe Sphere Easy'].mean()
+
+
 #printing data at 22nd may 2024
 print (scores[scores["Date"]=="22nd May 2024"]
 
 #printing the scores
 print(scores)
+print("The mean of Smooth Strafe Sphere Easy is: ", mean )
