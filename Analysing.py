9d6ffb96de3c4c13594c09b1e45af06d4817cb77
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-06-21 12:41:32 +0530

    creating graph

--- Analysing.py
+++ Analysing.py
@@ -1,4 +1,6 @@
 import pandas as pd #importing pandas
+import numpy as np #importing numpy
+import matplotlib.pyplot as plt
 
 #creating a variable that contains scores 
 scores=pd.read_csv('Aim Traning.csv')
@@ -6,9 +8,13 @@ scores=pd.read_csv('Aim Traning.csv')
 #calculating mean of 2nd coloum 
 mean=scores['Smooth Strafe Sphere Easy'].mean()
 
-
 #printing data at 22nd may 2024
 print (scores[scores["Date"]=="22nd May 2024"]
+
+#creating a graph of Smooth Strafe Sphere Easy
+x=np.array(scores[scores["Date"]])
+y=np.array(scores[scores["Smooth Strafe Sphere Easy")
+
 
 #printing the scores
 print(scores)
